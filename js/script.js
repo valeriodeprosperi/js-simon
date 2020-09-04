9162b298a72579cd75a7678658ed78e6716c1bd1
valerio <valerio.deprosperi@gmail.com>
2020-09-04 15:40:52 +0200

    creazione alert e num random problema con $ (document)

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Un alert espone 5 numeri casuali diversi.
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+$(document).ready(function() {
+// creo alert num casuali
+var numCasuali = numRandom();
+alert(numCasuali);
+// creo funzione numeri casuali da 1 a 100
+function numRandom(){
+  var arrayRandom = [];
+  for (var i = 0; i < 5; i++) {
+    var numero = Math.floor((Math.ramdom() * 10) + 1);
+    arrayRandom.push(numero);
+  }
+
+}
+});
