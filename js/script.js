5f96187c4b027bf24b29626f802297e7931472b1
valerio <valerio.deprosperi@gmail.com>
2020-09-04 15:55:55 +0200

    problema risolto + correzzione math random

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Un alert espone 5 numeri casuali diversi.
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
-$(document).ready(function() {
+
 // creo alert num casuali
 var numCasuali = numRandom();
 alert(numCasuali);
@@ -9,9 +9,9 @@ alert(numCasuali);
 function numRandom(){
   var arrayRandom = [];
   for (var i = 0; i < 5; i++) {
-    var numero = Math.floor((Math.ramdom() * 10) + 1);
+    var numero = Math.floor((Math.random() * 10) + 1);
     arrayRandom.push(numero);
   }
+  return arrayRandom;
 
 }
-});
